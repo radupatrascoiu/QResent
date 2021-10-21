7a4de602c92f97f0ad3269e4a936fce7b16c92f8
radupatrascoiu <patrascoiu.ion.radu@gmail.com>
2021-10-21 22:00:18 +0300

    Add skel for QResent

---
+++ backend/src/main/java/com/upb/qresent/QresentApplication.java
@@ -0,0 +1,13 @@
+package com.upb.qresent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QresentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QresentApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/upb/qresent/QresentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upb.qresent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QresentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
