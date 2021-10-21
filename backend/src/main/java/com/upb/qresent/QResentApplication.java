120176999ebdff3f4e6eb8c4921406ff1af1dbd8
radupatrascoiu <patrascoiu.ion.radu@gmail.com>
2021-10-21 22:09:29 +0300

    Fix name for the Project

--- backend/src/main/java/com/upb/qresent/QResentApplication.java
+++ backend/src/main/java/com/upb/qresent/QResentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QresentApplication {
+public class QResentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QresentApplication.class, args);
+		SpringApplication.run(QResentApplication.class, args);
 	}
 
 }
