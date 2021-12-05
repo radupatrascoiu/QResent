a52c080453a61af373f05206317b8673dad0be56
Apostol Alexandru <alexapostol0118@gmail.com>
2021-12-05 20:46:32 +0200

    coverage CourseService and PresenceListService

--- backend/src/test/java/com/upb/qresent/CourseServiceTest.java
+++ backend/src/test/java/com/upb/qresent/CourseServiceTest.java
@@ -1,27 +1,145 @@
 package com.upb.qresent;
 
 import com.upb.qresent.course.Course;
+import com.upb.qresent.course.CourseDto;
+import com.upb.qresent.course.CourseRepository;
 import com.upb.qresent.course.CourseService;
+import com.upb.qresent.user.User;
+import com.upb.qresent.user.UserRepository;
 import org.bson.types.ObjectId;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.mongodb.internal.connection.tlschannel.util.Util.assertTrue;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 public class CourseServiceTest {
+
+    @Mock
+    private CourseRepository courseRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    private CourseService courseService;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        courseService = new CourseService(courseRepository, userRepository);
+    }
+
     @Test
-    void test(){
-        Course course = new Course("Mate1",new ObjectId(),"nimic","nimic","",new HashSet<String>());
-        CourseService courseservicetest = null;
+    public void getCourseByID_allcases() {
+        Course course = new Course("Mate1", new ObjectId(), (short) 1, "nimic", "nimic", "nimic", new HashSet<String>());
         ObjectId id = new ObjectId();
         course.setId(id);
-        assertEquals(courseservicetest.getCourseByID(id).getName(),"Mate1");
+        courseRepository.save(course);
+        when(courseRepository.findById(anyString())).thenReturn(Optional.of(course));
+        Course actualCourse = courseService.getCourseByID(id);
 
+        assertEquals(course.getId(), actualCourse.getId());
+    }
+
+    @Test
+    public void enrolStudents_usernull() {
+        Course course = new Course("Mate1", new ObjectId(), (short) 1, "nimic", "nimic", "nimic", new HashSet<String>());
+        ObjectId id = new ObjectId();
+        course.setId(id);
+        courseRepository.save(course);
+        User user = null;
+
+        boolean check = courseService.enrollStudents(user, id.toString());
+
+        assertFalse(check);
+    }
+
+    @Test
+    public void enrolStudents_coursIdnull() {
+        Course course = new Course("Mate1", new ObjectId(), (short) 1, "nimic", "nimic", "nimic", new HashSet<String>());
+        ObjectId id = new ObjectId();
+        course.setId(id);
+        courseRepository.save(course);
+        User user = null;
+        boolean check = courseService.enrollStudents(user, null);
+
+        assertFalse(check);
+    }
+
+    @Test
+    public void enrolStudents_coursenotnull() {
+        Course course = new Course("Mate1", new ObjectId(), (short) 1, "nimic", "nimic", "nimic", new HashSet<String>());
+        ObjectId id = new ObjectId();
+        course.setId(id);
+        courseRepository.save(course);
+        User user = new User("Alex", "Student", "", "", new HashSet<>());
+        user.insertCourseIntoCourses(id);
+        when(courseRepository.findById(anyString())).thenReturn(Optional.of(course));
+        boolean check = courseService.enrollStudents(user, id.toString());
+
+        assertTrue(check);
+    }
+
+    @Test
+    public void enrolStudents_coursenotnull2() {
+        Course course = new Course("Mate1", new ObjectId(), (short) 1, "nimic", "nimic", "nimic", new HashSet<String>());
+        ObjectId id = new ObjectId();
+        course.setId(id);
+        courseRepository.save(course);
+        User user = new User("Alex", "Student", "", "", new HashSet<>());
+        when(courseRepository.findById(anyString())).thenReturn(Optional.of(course));
+        boolean check = courseService.enrollStudents(user, id.toString());
+
+        assertTrue(check);
+    }
+
+    @Test
+    public void enrolStudents_coursenull() {
+        Course course = new Course("Mate1", new ObjectId(), (short) 1, "nimic", "nimic", "nimic", new HashSet<String>());
+        ObjectId id = new ObjectId();
+        course.setId(id);
+        courseRepository.save(course);
+        User user = new User("Alex", "Student", "", "", new HashSet<>());
+        user.insertCourseIntoCourses(id);
+        when(courseRepository.findById(anyString())).thenReturn(Optional.of(course));
+        boolean check = courseService.enrollStudents(user, "nothing");
+
+        assertTrue(check);
+    }
+
+    @Test
+    public void getCoursesList_usernull() {
+        Course course = new Course("Mate1", new ObjectId(), (short) 1, "nimic", "nimic", "nimic", new HashSet<String>());
+        ObjectId id = new ObjectId();
+        course.setId(id);
+        courseRepository.save(course);
+        User user = new User("Alex", "Student", "", "", null);
+        assertNull(courseService.getCoursesList(null));
+    }
+
+    @Test
+    public void getCoursesList_usernotnull() {
+        Course course = new Course("Mate1", new ObjectId(), (short) 1, "nimic", "nimic", "nimic", new HashSet<String>());
+        ObjectId id = new ObjectId();
+        course.setId(id);
+        courseRepository.save(course);
+        User user = new User("Alex", "Student", "", "", new HashSet<>());
+        user.insertCourseIntoCourses(id);
+        when(courseRepository.findById(anyString())).thenReturn(Optional.of(course));
+        courseService.enrollStudents(user, id.toString());
+        List<CourseDto> list = courseService.getCoursesList(user);
+        assertEquals(user.getCourses().stream().toList(), list);
     }
 }
 
---
+++ backend/src/test/java/com/upb/qresent/PresenceListServiceTest.java
@@ -0,0 +1,114 @@
+package com.upb.qresent;
+
+
+import com.upb.qresent.course.Course;
+import com.upb.qresent.course.CourseRepository;
+import com.upb.qresent.course.CourseService;
+import com.upb.qresent.presentList.PresenceList;
+import com.upb.qresent.presentList.PresenceListRepository;
+import com.upb.qresent.presentList.PresenceListService;
+import com.upb.qresent.qrCode.QRCode;
+import com.upb.qresent.qrCode.QRCodeRepository;
+import org.bson.types.ObjectId;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PresenceListServiceTest {
+
+    @Mock
+    private CourseRepository courseRepository;
+
+    @Mock
+    private PresenceListRepository presenceListRepository;
+
+    @Mock
+    private QRCodeRepository qrCodeRepository;
+
+    private PresenceListService presenceListService;
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        presenceListService = new PresenceListService(presenceListRepository,  courseRepository,  qrCodeRepository);
+    }
+
+    @Test
+    public void createPresenceList_null() {
+        Course course = new Course("Mate1", new ObjectId(), (short) 1, "nimic", "nimic", "nimic", new HashSet<String>());
+        ObjectId id = new ObjectId();
+        course.setId(id);
+        courseRepository.save(course);
+        assertNull(presenceListService.createPresenceList(new ObjectId()));
+    }
+
+    @Test
+    public void createPresenceList_notnull() {
+        Course course = new Course("Mate1", new ObjectId(), (short) 1, "nimic", "nimic", "nimic", new HashSet<String>());
+        ObjectId id = new ObjectId();
+        course.setId(id);
+        courseRepository.save(course);
+        when(courseRepository.findById(anyString())).thenReturn(Optional.of(course));
+        when(presenceListRepository.save(any(PresenceList.class))).thenReturn(new PresenceList(new ObjectId(),new ObjectId(),new ObjectId(),new Date(),new Date(),null));
+        when(qrCodeRepository.save(any(QRCode.class))).thenReturn(new QRCode());
+        assertNotNull(presenceListService.createPresenceList(new ObjectId()));
+    }
+
+    @Test
+    public void getPresenceListByID() {
+        Course course = new Course("Mate1", new ObjectId(), (short) 1, "nimic", "nimic", "nimic", new HashSet<String>());
+        ObjectId id = new ObjectId();
+        course.setId(id);
+        courseRepository.save(course);
+        assertNull(presenceListService.getPresenceListByID(id));
+    }
+
+    @Test
+    public void updatePresenceList() {
+        Course course = new Course("Mate1", new ObjectId(), (short) 1, "nimic", "nimic", "nimic", new HashSet<String>());
+        ObjectId id = new ObjectId();
+        course.setId(id);
+        courseRepository.save(course);
+        PresenceList list = null;
+        assertNull(presenceListService.updatePresenceList(list));
+    }
+
+    @Test
+    public void refreshAndGetPresenceListByIDisnull() {
+        Course course = new Course("Mate1", new ObjectId(), (short) 1, "nimic", "nimic", "nimic", new HashSet<String>());
+        ObjectId id = new ObjectId();
+        course.setId(id);
+        courseRepository.save(course);
+        PresenceList list = null;
+        assertNull(presenceListService.refreshAndGetPresenceListByID(id));
+    }
+
+    @Test
+    public void refreshAndGetPresenceListByIDisnotnull() {
+        ObjectId id = new ObjectId();
+        PresenceList list = new PresenceList(new ObjectId(),new ObjectId(),new ObjectId(),new Date(),new Date(),null);
+        when(presenceListRepository.findById(anyString())).thenReturn(Optional.of(list));
+//        when(qrCodeRepository.save(any(QRCode.class))).thenReturn(new QRCode());
+        assertNotNull(presenceListService.refreshAndGetPresenceListByID(id));
+    }
+
+    @Test
+    public void updateQR() {
+        PresenceList list = new PresenceList(new ObjectId(),new ObjectId(),new ObjectId(),new Date(),new Date(),null);
+        when(qrCodeRepository.save(any(QRCode.class))).thenReturn(new QRCode());
+        assertNull(presenceListService.updateQR(list));
+    }
+
+}
